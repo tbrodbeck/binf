2dc70f3fcf23cf1136049a0d450421466eeb0dfd
rovoki <ronja.von.kittlitz@gmail.com>
2017-04-10 22:25:59 +0200

    update

--- src/FibonacciPrint.java
+++ src/FibonacciPrint.java
@@ -1,55 +1,27 @@
 /**
  * @author Ronja von Kittlitz, Tillmann Brodbeck
  * @version 10.04.17.
+ * Implementieren Sie anschließend die Klasse FibonacciPrint. Wenn die Klasse über die Kom- mandozeile aufgerufen wird,
+ * soll ihr als Parameter genau eine ganze Zahl n übergeben werden. Diese wird in Java automatisch in das String[] der
+ * main Methode übergeben. Von FibonacciPrint sollen alle Fibonacci-Zahlen von f (0) bis f (n) wie folgt formatiert auf
+ * der Kommandozeile ausgegeben werden:
+ *| n |  f(n)  |
+ *+---+--------+
+ *|  0|       0|
  */
- */
+
 public class FibonacciPrint {
-    /**
-     * If one argument with a number n is given, prints a list of Fibonacci
-     * numbers, starting with f(0) to f(n).
-     *
-     * @param args
-     * args[0] must contain a number or -help
-     */
-    public static void main(String[] args) {
-        if (args.length == 1) {
-            if (args[0].matches("-help")) {
-                printHelp();
-            } else {
-                int n = Integer.parseInt(args[0]);
-                if (n > 1) {
-                    output(n);
-                } else {
-                    printHelp();
-                }
-            }
-        } else {
-            printHelp();
-        }
-    }
-    /**
-     * Prints the usage of the program on the standard console.
-     */
-    private static void printHelp() {
-        System.out.print("Usage: java FibonacciPrint [number] \n");
-        System.out.print("[number] is a positive number > 1. ");
-        System.out.print("print all Fibonacci numbers from 0 to [number] \n");
-    }
-    /**
-     * Prints all Fibonacci numbers from 0 to n on the standard console.
-     *
-     * @param n
-     * number to which the Fibonacci numbers should be printed.
-     */
-    private static void output(int n) {
-        Fibonacci f = new Fibonacci();
-        System.out.println("| n | f(n) |");
-        4
+    public static void main(String[] args){
+        if(args.length!= 1) throw new RuntimeException("Es darf nur eine Zahl übergeben werden.");
+        int n = Integer.parseInt(args[0]);
+        if (n< 2) throw new RuntimeException("n < 2");
+        System.out.println("| n |  f(n)  |");
         System.out.println("+---+--------+");
-        System.out.println("| 0| 0|");
-        System.out.println("| 1| 1|");
-        for (int i = 0; i < n - 1; i++) {
-            System.out.printf("|%3d|%8d|%n", i + 2, f.next());
+        System.out.println("|  0|       0|");
+        System.out.println("|  1|       1|");
+        Fibonacci fib = new Fibonacci();
+        for(int i = 2; i <= n; n++){
+            System.out.printf("|%3d|%8d|", i, fib.next());
         }
     }
 }
