b0e6c30097d0d2b2d9c435ddead9ea4699c06341
rovoki <ronja.von.kittlitz@gmail.com>
2017-04-11 11:34:11 +0200

    update

--- src/Fraglich.java
+++ src/Fraglich.java
@@ -12,21 +12,18 @@ public class Fraglich {
         initialize();
 
         /*** Stelle 3 **
-         a = 23; b nicht vergeben; c = {16, 4, 2, 9} */
-        // a müsste doch 5 sein?
+         a = 5; b nicht vergeben; c = {16, 4, 2, 9} */
 
         for (int b = -1; b < 1; b += 3) {
 
             /*** Stelle 4 **
-             a = 23; b = -1; c = {16, 4, 2, 9} */
-            // a = 5
+             a = 5; b = -1; c = {16, 4, 2, 9} */
             a /= 2;
             c[b + 2] -= c[b + 2];
         }
 
         /*** Stelle 5 **
-         a = 11; b nicht vergeben; c = {16, 0, 2, 9} */
-        // a sollte 2 sein
+         a = 2; b nicht vergeben; c = {16, 0, 2, 9} */
 
         for (a = 2; a < 3; a++) {
 
@@ -44,8 +41,7 @@ public class Fraglich {
         int b = method(a + c[a - 3]);
 
         /*** Stelle 9
-         a = 3; b = 3; c = {16, 0, 0, 9} ***/
-        // b ist 23
+         a = 3; b = 23; c = {16, 0, 0, 9} ***/
 
         b = 7 + method(++a, c);
 
@@ -53,11 +49,9 @@ public class Fraglich {
          a = 4; b = 12; c = {8, 0, 0, 9} ***/
 
         a = method(method(method(a), new int[]{b, a, c[0], c[2], c[3], c[1]}));
-        //6,4,8,0,9,0
 
         /*** Stelle 15
-         a = 4; b = 12; c = {8, 0, 0, 9} ***/
-        // a ist 23
+         a = 23; b = 12; c = {8, 0, 0, 9} ***/
     }
 
     static void initialize() {
@@ -67,21 +61,17 @@ public class Fraglich {
         a = 23;
 
         /*** Stelle 2 **
-         a = 23; b = 0; c = {16, 4, 2, 9} */
-        // c gibts doch hier gar nicht?
+         a = 23; b = 0; c nicht vorhanden */
     }
 
     protected static int method(int b) {
 
         /*** Stelle 8
-         a = 3; b = 19; c = {16, 0, 0, 9}
-         // c gibts nicht und a = 23
+         a = 23; b = 19; c nicht vorhanden
          Stelle 12
-         a = 4; b = 4; c = {8, 0, 0, 9}
-         // a ist 23 und c nicht da
+         a = 23; b = 4; c nicht vorhanden
          Stelle 14
-         a = 4; b = 5; c = {8, 0, 0, 9}
-         // siehe oben
+         a = 23; b = 5; c nicht vorhanden
          ***/
 
         return a;
@@ -95,11 +85,9 @@ public class Fraglich {
         c = new int[4];
 
         /*** Stelle 10
-         a = 5; b = 3; c = {0, 0, 0, 0}
-         b gibts nicht
+         a = 5; b nicht vorhanden; c = {0, 0, 0, 0}
          Stelle 13
-         a = 5; b = 3; c = {0, 0, 0, 0} ***/
-        // b gibts nicht
+         a = 5; b = nicht vorhanden; c = {0, 0, 0, 0} ***/
 
 
         return a;
