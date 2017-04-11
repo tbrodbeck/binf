18eddab43d3f4a1dedd790a1ba6b751bc6e0be58
rovoki <ronja.von.kittlitz@gmail.com>
2017-04-11 10:40:02 +0200

    update

--- src/Fraglich.java
+++ src/Fraglich.java
@@ -13,17 +13,20 @@ public class Fraglich {
 
         /*** Stelle 3 **
          a = 23; b nicht vergeben; c = {16, 4, 2, 9} */
+        // a müsste doch 5 sein?
 
         for (int b = -1; b < 1; b += 3) {
 
             /*** Stelle 4 **
              a = 23; b = -1; c = {16, 4, 2, 9} */
+            // a = 5
             a /= 2;
             c[b + 2] -= c[b + 2];
         }
 
         /*** Stelle 5 **
          a = 11; b nicht vergeben; c = {16, 0, 2, 9} */
+        // a sollte 2 sein
 
         for (a = 2; a < 3; a++) {
 
@@ -37,10 +40,12 @@ public class Fraglich {
         /*** Stelle 7 **
          a = 3; b nicht vergeben; c = {16, 0, 0, 9} */
 
+
         int b = method(a + c[a - 3]);
 
         /*** Stelle 9
          a = 3; b = 3; c = {16, 0, 0, 9} ***/
+        // b ist 23
 
         b = 7 + method(++a, c);
 
@@ -48,9 +53,11 @@ public class Fraglich {
          a = 4; b = 12; c = {8, 0, 0, 9} ***/
 
         a = method(method(method(a), new int[]{b, a, c[0], c[2], c[3], c[1]}));
+        //6,4,8,0,9,0
 
         /*** Stelle 15
          a = 4; b = 12; c = {8, 0, 0, 9} ***/
+        // a ist 23
     }
 
     static void initialize() {
@@ -61,16 +68,20 @@ public class Fraglich {
 
         /*** Stelle 2 **
          a = 23; b = 0; c = {16, 4, 2, 9} */
+        // c gibts doch hier gar nicht?
     }
 
     protected static int method(int b) {
 
         /*** Stelle 8
          a = 3; b = 19; c = {16, 0, 0, 9}
+         // c gibts nicht und a = 23
          Stelle 12
          a = 4; b = 4; c = {8, 0, 0, 9}
+         // a ist 23 und c nicht da
          Stelle 14
          a = 4; b = 5; c = {8, 0, 0, 9}
+         // siehe oben
          ***/
 
         return a;
@@ -85,8 +96,11 @@ public class Fraglich {
 
         /*** Stelle 10
          a = 5; b = 3; c = {0, 0, 0, 0}
+         b gibts nicht
          Stelle 13
          a = 5; b = 3; c = {0, 0, 0, 0} ***/
+        // b gibts nicht
+
 
         return a;
     }
