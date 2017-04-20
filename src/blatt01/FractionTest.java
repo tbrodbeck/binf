5cc4faba7395147451d2773c5673d8be633d949a
Till Brodbeck <t.b@riseup.net>
2017-04-20 10:54:23 +0200

    rückgängig

---
+++ src/blatt01/FractionTest.java
@@ -0,0 +1,53 @@
+package blatt01;
+
+/**
+ * @author Ronja von Kittlitz, Tillmann Brodbeck
+ * @version 10.04.17.
+ *
+ * Testen Sie Ihre neue Klasse mit Hilfe einer separaten Testklasse. Das Testprogramm soll einige In- stanzen der Klasse
+ * Fraction erzeugen und jede der programmierten Operationen mindestens ein- mal testen. Es soll automatisiert eine
+ * Ausgabe erfolgen, ob der jeweilige Test erfolgreich war oder nicht.
+ */
+public class FractionTest {
+    public static void main(String[] args) {
+        Fraction f1 = new Fraction(3, 6);
+        Fraction f2 = new Fraction(8);
+        boolean fehler = false;
+        //1. Complier und toString testen
+        if (!f1.toString().equals("1/2")) {
+            System.out.print("Fehler beim Konstruktor mit Nenner/toString" + f1.toString());
+            fehler = true;
+        }
+        //2. Complier und toSting testen
+        if (!f2.toString().equals("8/1")) {
+            System.out.print("Fehler beim Konstruktor ohne Nenner/toString" + f2.toString());
+            fehler = true;
+        }
+        //multiply() mit Faktor testen
+        Fraction f3 = f1.multiply(3);
+        if(!f3.toString().equals("3/2")) {
+            System.out.println("Fehler beim multiplizieren mit Faktor" + f3.toString());
+            fehler = true;
+        }
+        //multiply() mit anderem Bruch testen
+        Fraction f4 = f1.multiply(f2);
+        if(!f4.toString().equals("4/1")){
+            System.out.println("Fehler beim multiplizieren mit Bruch");
+            fehler = true;
+        }
+        //mulitply() mit Liste von Brüchen
+        Fraction f5 = new Fraction(7,4);
+        Fraction f6 = f1.multiply(f4, f5);
+        if(!f6.toString().equals("7/2")) {
+            System.out.println("Fehler beim multiplizieren von mehreren Brüchen" + f6.toString());
+            fehler = true;
+        }
+        //divide
+        Fraction f7 = f1.divide(f2);
+        if(!f7.toString().equals("1/16")) {
+            System.out.println("Fehler bei divide");
+            fehler = true;
+        }
+        if (!fehler) System.out.println("Kein Fehler");
+    }
+}
---
+++ src/blatt01/Fraglich.java
@@ -0,0 +1,106 @@
+package blatt01;
+
+public class Fraglich {
+
+    static int a;
+
+    public static void main(String[] args) {
+        int a = 5;
+        int[] c = {16, 4, 2, 9};
+
+        /*** Stelle 1 **
+         a = 5; b ist nicht vergeben; c = {16, 4, 2, 9}; */
+
+        initialize();
+
+        /*** Stelle 3 **
+         a = 5; b nicht vergeben; c = {16, 4, 2, 9} */
+
+        for (int b = -1; b < 1; b += 3) {
+
+            /*** Stelle 4 **
+             a = 5; b = -1; c = {16, 4, 2, 9} */
+            a /= 2;
+            c[b + 2] -= c[b + 2];
+        }
+
+        /*** Stelle 5 **
+         a = 2; b nicht vergeben; c = {16, 0, 2, 9} */
+
+        for (a = 2; a < 3; a++) {
+
+            int b = 2;
+            c[a] -= c[b];
+
+            /*** Stelle 6 **
+             a = 2; b = 2; c = {16, 0, 0, 9} */
+        }
+
+        /*** Stelle 7 **
+         a = 3; b nicht vergeben; c = {16, 0, 0, 9} */
+
+
+        int b = method(a + c[a - 3]);
+
+        /*** Stelle 9
+         a = 3; b = 23; c = {16, 0, 0, 9} ***/
+
+        b = 7 + method(++a, c);
+
+        /*** Stelle 11
+         a = 4; b = 12; c = {8, 0, 0, 9} ***/
+
+        a = method(method(method(a), new int[]{b, a, c[0], c[2], c[3], c[1]}));
+
+        /*** Stelle 15
+         a = 23; b = 12; c = {8, 0, 0, 9} ***/
+    }
+
+    static void initialize() {
+
+        int b = 0;
+
+        a = 23;
+
+        /*** Stelle 2 **
+         a = 23; b = 0; c nicht vorhanden */
+    }
+
+    protected static int method(int b) {
+
+        /*** Stelle 8
+         a = 23; b = 19; c nicht vorhanden
+         Stelle 12
+         a = 23; b = 4; c nicht vorhanden
+         Stelle 14
+         a = 23; b = 5; c nicht vorhanden
+         ***/
+
+        return a;
+    }
+
+    private static int method(int a, int[] c) {
+
+        a++;
+        c[0] /= 2;
+        c[0] = c[0] + c[1];
+        c = new int[4];
+
+        /*** Stelle 10
+         a = 5; b nicht vorhanden; c = {0, 0, 0, 0}
+         Stelle 13
+         a = 5; b = nicht vorhanden; c = {0, 0, 0, 0} ***/
+
+
+        return a;
+    }
+
+}
+
+/*
+In diesem Programm und allgemeint in Java wird Call by Value verwendet. Das
+heißt, dass Methoden-Parameter anhand ihrer übergebenen Werte aufgerufen werden.
+In anderen Programmiersprachen gibt es auch Call by Reference und das meint,
+dass die Methode direkt Zugriff auf die ihr übergebenen Variablen hat, weil die
+Variablen und nicht nur die Werte übergeben werden.
+ */
