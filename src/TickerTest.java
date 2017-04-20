7affde51aba4b52669cab8a8118a5a208c3d9b67
Till Brodbeck <t.b@riseup.net>
2017-04-20 02:07:09 +0200

    Aufgabe 1

--- src/TickerTest.java
+++ src/TickerTest.java
@@ -1,7 +1,10 @@
 /**
  * @author Ronja von Kittlitz, Tillmann Brodbeck
  * @version 20.04.17.
- * 2. Implementieren Sie zusätzlich einen Testklasse, die durch die Erzeugung von Objekten des Typs Company und expliziten Anstoß des Garbage Collectors dessen Arbeitsweise im Zusammenspiel mit Konstruktoren und Destruktoren verdeutlicht. Erklären Sie Ihrem Tutor wie Sie vorgegangen sind und was man aus den Ergebnissen Ihrer Testklasse schließen kann.
+ * 2. Implementieren Sie zusätzlich einen Testklasse, die durch die Erzeugung von Objekten des Typs
+ * Company und expliziten Anstoß des Garbage Collectors dessen Arbeitsweise im Zusammenspiel mit
+ * Konstruktoren und Destruktoren verdeutlicht. Erklären Sie Ihrem Tutor wie Sie vorgegangen sind und was
+ * man aus den Ergebnissen Ihrer Testklasse schließen kann.
  */
 public class TickerTest {
 
@@ -14,7 +17,9 @@ public class TickerTest {
         testMethode();
         nutella.changeStockPrice(66.6);
         fussball.changeStockPrice(0.1);
-        System.out.println("\nErwarteter String: \n+++Java Inc. 555.6+++Zahnpasta in your Face 10.0+++Nutella GmBH 66.6+++Fussball ABC 0.1");
+        fussball.finalize();
+
+        System.out.println("\nErwarteter String: \n+++Java Inc. 555.6+++Zahnpasta in your Face 10.0+++Nutella GmBH 66.6+++Fussball ABC 0.1+++Insolvenz von Fussball ABC bekanntgegeben.");
 
     }
 
@@ -22,5 +27,6 @@ public class TickerTest {
         Company mitsubishi = new Company("Mitsubishi CoKG");
         Company zahnpasta = new Company("Zahnpasta in your Face");
         zahnpasta.changeStockPrice(10.0);
+        zahnpasta = null;
     }
 }
