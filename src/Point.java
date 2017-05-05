47ab55392fd9f5cfc99e23afa528d1f75ddc8f3b
Till Brodbeck <t.b@riseup.net>
2017-05-05 22:07:27 +0200

    Korrekturen

--- src/Point.java
+++ src/Point.java
@@ -37,7 +37,7 @@ public class Point extends Geometry implements Comparable{
     public Geometry encapsulate(Geometry other) {
         if (dimensions() != other.dimensions()) return null;
         if (other instanceof Point){
-            if ((Point)other.getCoords() == getCoords())
+            if (((Point)other).getCoords() == getCoords())
                 return this;
             return new Rectangle(this, (Point)other);
         }
