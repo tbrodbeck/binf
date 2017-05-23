49950d5ed7d887506b3ab88b6753088eff95823d
Till Brodbeck <t.b@riseup.net>
2017-05-23 09:43:47 +0200

    3 bisschen was

--- src/blatt05/Heap.java
+++ src/blatt05/Heap.java
@@ -9,35 +9,62 @@ import java.util.Comparator;
  *
  * Implementieren Sie einen typsicheren Heap.
  *
+ * Die Ordnung der eingefügten Schlüssel-Elemente soll entweder dadurch gegeben sein, dass alle Schlüssel
+ * das Interface java.lang.Comparable implementieren oder dass beim Erzeugen eines Heaps ein passender
+ * java.util.Comparator mitgegeben wird. Mit Ihrer Implementation sollen beide Varianten abgedeckt werden.
+ *
  * @author Ronja von Kittlitz, Tillmann Brodbeck
  * @version 16.05.17
  */
 public class Heap<T extends Comparable>{
 
-    private T[] h;
+    private Object[] h;
     private Comparator<T> comparator;
 
+    /**
+     * Wird der Heap also mit einem Comparator erzeugt, wird die Ordnung anhand dieses Comparator deﬁniert.
+     * @param comparator
+     */
     public Heap(Comparator<T> comparator) {
         this.comparator = comparator;
-        //h = new T[0];
+        h = new Object[0];
     }
 
-//    public Heap() {
-//        h = new T[0];
-//    }
+    /**
+     * Wurde kein Comparator mit angegeben, soll die Heap-Implementation davon ausgehen, das alle
+     * eingefügten Instanzen vom Typ Comparable sind.
+     */
+    public Heap() {
+        h = new Object[0];
+    }
 
+    /**
+     * Mit den Methoden eines Heaps soll man Objekte in einen Heap einfügen,
+     * @param e Objekt
+     */
     public void insert(T e) {
+
     }
 
     public boolean empty() {
         return (h.length == 0);
     }
 
-//    public T returnFirst(){
-//
-//    }
 
+    /**
+     * das kleinste Objekt löschen
+     * @return kleinstes Objekt
+     */
     public T deleteFirst() {
+        return null;
+    }
+
+
+    /**
+     * oder das kleinste Objekt ermitteln können.
+     * @return kleinstes Objekt
+     */
+    public T returnFirst(){
         return null;
     }
 }
