6777d90c5fa86a8179d0a5311cc5719529006f57
Till Brodbeck <t.b@riseup.net>
2017-06-15 16:08:34 +0200

    A1 angefangen

---
+++ src/blatt08/Arena.java
@@ -0,0 +1,27 @@
+package blatt08;
+
+/**
+ * Bei den jährlich stattﬁndenden Hungerspielen treten 24 Tribute aus den 12 Distrikten des ﬁktiven Landes Panem in
+ * einer künstlichen Arena gegeneinander an. Bei der 75. Durchführung der Hungerspiele ist die kreisrunde Arena
+ * analog zu einer Uhr in zwölf gleich große Bereiche unterteilt. Die Mitte der Arena habe die Position (0,0) und
+ * die Arena einen Durchmesser von 3 Meilen. Je nach Uhrzeit wird ein Bereich aktiv und in ihm werden tödliche
+ * Fallen freigeschaltet. Beispielsweise ist von 12 bis 1 Uhr der Bereich im Winkel von 0 ◦ bis 30 ◦ aktiv, wobei
+ * 0 ◦ die Linie von der Mitte bis zur Position (0,1.5) anzeigt. Ein Tribut wird fast sicher umkommen, wenn er sich
+ * in einem aktiven Bereich aufhält oder die Arena verlässt.
+ *
+ * @author Ronja von Kittlitz, Tillmann Brodbeck
+ * @version 15.06.17
+ */
+public class Arena {
+
+    /**
+     * Schreiben sie eine Java-Klasse Arena, die die Methode int getArea(double x, double y) enthält und zurück
+     * gibt, in welchem der zwölf Bereiche sich ein Tribut mit der Position (x,y) beﬁndet.
+     * @param x
+     * @param y
+     * @return
+     */
+    public int getArea(double x, double y) {
+        return -1;
+    }
+}
