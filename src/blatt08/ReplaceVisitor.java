09489905a1532c9a485ea08932d88da2fc2a43fa
rovoki <ronja.von.kittlitz@gmail.com>
2017-06-19 19:04:15 +0200

    Fertisch

--- src/blatt08/ReplaceVisitor.java
+++ src/blatt08/ReplaceVisitor.java
@@ -21,11 +21,23 @@ public class ReplaceVisitor implements  FileVisitor {
         this.root = root;
     }
 
+    /**
+     * wird nicht benutzt
+     * @param dir
+     *           the directory that has been visited
+     * @return CONTINUE
+     */
     @Override
     public FileVisitResult postVisitDirectory(File dir) {
         return FileVisitResult.CONTINUE;
     }
 
+    /**
+     *
+     * @param dir
+     *           the directory that may be visited
+     * @return ob directory rekursiv durchlaufen werden soll
+     */
     @Override
     public FileVisitResult preVisitDirectory(File dir) {
         if (recursive || this.root.equals(dir)) {
@@ -35,13 +47,33 @@ public class ReplaceVisitor implements  FileVisitor {
         }
     }
 
+    /**
+     *
+     * @param dir
+     *           the directory that could not be visited
+     * @return
+     */
     @Override
     public FileVisitResult visitFailed(File dir) {
-        return null;
+        System.out.println(dir + " konnte nicht bearbeitet werden");
+        return FileVisitResult.CONTINUE;
     }
 
+    /**
+     * Durchläuft eine Datei:
+     * Durchlaufen Sie jede dieser Dateien vollständig und ersetzen Sie jedes Vorkom- men eines vom Nutzer gegebenen
+     * regulären Ausdrucks durch eine vom Nutzer gegebene Zeichenkette.
+     *Da reguläre Ausdrücke auch Zeilenumbrüche enthalten können, müssen Sie die jewei- lige Datei vollständig in einen
+     *String umwandeln. Das Zeichen, dass einen Zei- lenumbruch in Ihrem Betriebssystem definiert, erhalten Sie durch
+     *den Aufruf von System.getProperty("line.separator"). Schreiben Sie den veränderten String am Ende wieder zurück
+     *in die Datei.
+     * @param file
+     *           the file that is visited
+     * @return CONTINUE
+     */
     @Override
     public FileVisitResult visitFile(File file) {
+        //Inhalt der Datei -> String
         BufferedReader r;
         String nl = System.getProperty("line.separator");
         try {
@@ -65,6 +97,7 @@ public class ReplaceVisitor implements  FileVisitor {
                 e.printStackTrace();
             }
         }
+        // im String Search durch Replace ersetzen
         Matcher m = search.matcher(text);
         StringBuffer sb = new StringBuffer();
         while (m.find()) {
@@ -73,6 +106,7 @@ public class ReplaceVisitor implements  FileVisitor {
         m.appendTail(sb);
         String newText = sb.toString();
         FileWriter w = null;
+        //String in File zurück schreiben
         file.delete();
         try {
             file.createNewFile();
@@ -97,3 +131,6 @@ public class ReplaceVisitor implements  FileVisitor {
         return FileVisitResult.CONTINUE;
     }
 }
+
+//Fragen: bessere Lösung für Löschen und neu erstellen für überschreiben
+//
